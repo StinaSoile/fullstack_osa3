b5675d20285a86e34094d853b735e6748fea6103
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-04-12 19:22:28 +0300

    3.8 done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 app.use(express.json())
+// app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
