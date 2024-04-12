b292c94fa076888e06c99a8d9edacdf9df7a28b9
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-04-12 18:39:47 +0300

    logger added

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+
+    console.log('Params:  ', request.params)
+
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
 
 let persons = [
     {
@@ -89,3 +100,8 @@ const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
